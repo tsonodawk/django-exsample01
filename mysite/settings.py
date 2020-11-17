7e8aa7484466798dcf607b274cfe16cba5e74bce
Toshiro Sonoda <t.sonoda.wk@gmail.com>
2020-11-17 17:22:12 +0900

    ALLOWHOST変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'isyuzcpe&bo^uc60!u9t58ljeh!0y=657e^2)58^lg(b&)%5r@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
